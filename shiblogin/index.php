d6b59ad34b6fc3e99c378218649d8a780ead5c07
Oleg Stikhin <oleg.stikhin@aalto.fi>
2017-07-25 12:22:08 +0300

    added ID

--- shiblogin/index.php
+++ shiblogin/index.php
@@ -9,17 +9,21 @@
       require 'secret_key.php';
       $url = "/shibbolethLogin";
       
-      $eppn = $_SERVER['eppn'];
-      
-      $user->eppn = $eppn;
+      $user->eppn = $_SERVER['eppn'];
       $user->code = $secret_key;
       $user->time = time();
-      $data = $user->eppn . ';' . $user->time . ';' . $user->code;
+      $user->cn = $_SERVER['common-name'];
+      $user->id = $_SERVER['unique-code'];
+      $user->mail = $_SERVER['mail'];
+      $data = $user->eppn . ';' . $user->id . ';' . $user->time . ';' . $user->code;
       $data_hash = hash("sha256", $data);
     ?>
     <form id="user_data" action="/shibbolethLogin" method="post">
       <input type="hidden" name="user" value='<?php echo $user->eppn; ?>'>
       <input type="hidden" name="time" value='<?php echo $user->time; ?>'>
+      <input type="hidden" name="personal_code" value='<?php echo $user->id; ?>'>
+      <input type="hidden" name="name" value='<?php echo $user->cn; ?>'>
+      <input type="hidden" name="mail" value='<?php echo $user->mail; ?>'>
       <input type="hidden" name="hash" value="<?php echo $data_hash; ?>">
     </form>
 
